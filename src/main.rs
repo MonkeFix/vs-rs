cb7e5543ca65658f0fc7a021c674b34b28087316
Sergey Gulin <sergeygulin96@gmail.com>
2024-01-14 03:40:19 +0300

    add movement plugin

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,17 @@ use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 
 mod tilemap;
+mod movement;
 
 use tilemap::TileMapPlugin;
+use movement::MovementPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(TileMapPlugin)
+        .add_plugins(MovementPlugin)
         .add_systems(Startup, (spawn_camera, spawn_player))
-        .add_systems(
-            Update,
-            (
-                movement,
-                move_player.after(handle_player_input),
-            )
-        )
         .run();
 }
 
@@ -115,50 +111,5 @@ fn move_player(
                 position.0 = position.0 + velocity.0 * PLAYER_SPEED;
             }
         }
-    }
-}
-
-
-pub fn movement(
-    time: Res<Time>,
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut Transform, &mut OrthographicProjection), With<Camera>>,
-) {
-    for (mut transform, mut ortho) in query.iter_mut() {
-        let mut direction = Vec3::ZERO;
-
-        if keyboard_input.pressed(KeyCode::Left) {
-            direction -= Vec3::new(1.0, 0.0, 0.0);
-        }
-
-        if keyboard_input.pressed(KeyCode::Right) {
-            direction += Vec3::new(1.0, 0.0, 0.0);
-        }
-
-        if keyboard_input.pressed(KeyCode::Up) {
-            direction += Vec3::new(0.0, 1.0, 0.0);
-        }
-
-        if keyboard_input.pressed(KeyCode::Down) {
-            direction -= Vec3::new(0.0, 1.0, 0.0);
-        }
-
-        if keyboard_input.pressed(KeyCode::Z) {
-            ortho.scale += 0.1;
-        }
-
-        if keyboard_input.pressed(KeyCode::X) {
-            ortho.scale -= 0.1;
-        }
-
-        if ortho.scale < 0.5 {
-            ortho.scale = 0.5;
-        }
-
-        let z = transform.translation.z;
-        transform.translation += time.delta_seconds() * direction * 500.;
-        // Important! We need to restore the Z values when moving the camera around.
-        // Bevy has a specific camera setup and this can mess with how our layers are shown.
-        transform.translation.z = z;
     }
 }
