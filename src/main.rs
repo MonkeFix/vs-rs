57851ba4d392e8f7ad1620d297338f5bf607849c
Grigory Alfyorov <loonacuse@gmail.com>
2024-01-24 01:30:54 +0500

    Change default resolution to 1600x900

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,19 @@ use tilemap::TileMapPlugin;
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins)
-        .add_plugins(TileMapPlugin)
-        .add_plugins(MovementPlugin)
-        .add_plugins(PlayerPlugin)
-        .add_systems(Startup, spawn_camera);
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "VS-RS".into(),
+            resolution: (1600., 900.).into(),
+            fit_canvas_to_parent: true,
+            ..default()
+        }),
+        ..default()
+    }))
+    .add_plugins(TileMapPlugin)
+    .add_plugins(MovementPlugin)
+    .add_plugins(PlayerPlugin)
+    .add_systems(Startup, spawn_camera);
 
     #[cfg(debug_assertions)]
     app.add_plugins(WorldInspectorPlugin::new());
